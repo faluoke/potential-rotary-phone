e94546a189d523711dd1aa92982c065d5035ed55
Faruk <15935486+faluoke@users.noreply.github.com>
2018-07-30 17:41:16 -0600

    Delete parkingCalc.java

--- parkingCalc.java
+++
@@ -1,8 +0,0 @@
-//parking calculation
-import java.util.Scanner;
-
-System.out.println("Welcome, please enter hr of parking\nParking is $2/hr")
-int hrs = input.nextLine();
-Parking myParking = new Parking(hrs);
-myParking.setPrice();
-System.out.println(myParking.getPrice()";
