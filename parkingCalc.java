96d456907a558170c9c3f719271497c3363fb28e
Faruk <15935486+faluoke@users.noreply.github.com>
2018-07-29 21:49:09 -0600

    Calculation for parking will be in main

---
+++ parkingCalc.java
@@ -0,0 +1,8 @@
+//parking calculation
+import java.util.Scanner;
+
+System.out.println("Welcome, please enter hr of parking\nParking is $2/hr")
+int hrs = input.nextLine();
+Parking myParking = new Parking(hrs);
+myParking.setPrice();
+System.out.println(myParking.getPrice()";
